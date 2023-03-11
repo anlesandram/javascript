dceb6fac34e77d7455a168fb3d3524c33ab99e85
Angela Andrade <anlesandram@gmail.com>
2023-03-11 10:44:48 -0500

    read file

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-const isOdd = require('is-odd')
+const fs = require('fs');
+const path = require('path');
 
-console.log(isOdd(3))
+const processFile = async(...args) => {
+    const filePath = path.resolve(`${__dirname}/archivo.txt`)
+    if(args[0] == "R"){
+        const data = await fs.promises.readFile(filePath, 'utf-8');
+        console.log(data)
+        
+    }else {
+        await fs.promises.writeFile(filePath, args[1]);
+    }
+}
 
+const readFile = async() => {
+    try{
+        const filePath = path.resolve(`${__dirname}/archivo.txt`);
+        const data = await fs.promises.readFile(filePath, 'utf-8');
+
+        console.log(data)
+    }catch(error)
+    {
+        console.log(error);
+    }
+}
+
+
+const writeFile = async(texto) =>{
+    try{
+        console.log(texto)
+        const filePath = path.resolve(`${__dirname}/archivo.txt`);
+        await fs.promises.writeFile(filePath, texto);
+    }catch(error)
+    {
+        console.log(error);
+    }
+}
+
+processFile('R')
