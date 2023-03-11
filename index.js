985398b7598dd4fcf3abe26970e611242a09131b
Angela Andrade <anlesandram@gmail.com>
2023-03-11 11:09:44 -0500

    Implement get api

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fetchApi = require('./src/api');
 
-const processFile = async(...args) => {
-    const filePath = path.resolve(`${__dirname}/archivo.txt`)
-    if(args[0] == "R"){
-        const data = await fs.promises.readFile(filePath, 'utf-8');
-        console.log(data)
-        
-    }else {
-        await fs.promises.writeFile(filePath, args[1]);
-    }
-}
-
-const readFile = async() => {
-    try{
-        const filePath = path.resolve(`${__dirname}/archivo.txt`);
-        const data = await fs.promises.readFile(filePath, 'utf-8');
-
-        console.log(data)
-    }catch(error)
-    {
-        console.log(error);
-    }
-}
+fetchApi('https://rickandmortyapi.com/api/character')
 
 
-const writeFile = async(texto) =>{
-    try{
-        console.log(texto)
-        const filePath = path.resolve(`${__dirname}/archivo.txt`);
-        await fs.promises.writeFile(filePath, texto);
-    }catch(error)
-    {
-        console.log(error);
-    }
-}
-
-processFile('R')
