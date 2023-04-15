ac8b3b3c6fc210ca4fd83a9aa7a742dc1b5e6a6f
Angela Andrade <anlesandram@gmail.com>
2023-04-15 08:55:32 -0500

    Add validation

--- lab/src/validation/productScheme.js
+++ lab/src/validation/productScheme.js
@@ -5,7 +5,7 @@ const productScheme = Joi.object({
   name: Joi.string().required(),
   description: Joi.string().required(),
   price: Joi.number().required(),
-  quantity: Joi.string().required(),
+  quantity: Joi.number().integer().required(),
   category: Joi.string().required(),
 });
 
